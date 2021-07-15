86ca99e0917ad9aaf3aee3d38c8d7a69e4b49e00
Davi Gonçalves Valério <al.davigvalerio@gmail.com>
2021-07-15 15:53:34 -0300

    Class 128 - Diving into "Forward Refs"

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useImperativeHandle, useRef } from 'react';
 import classes from './Input.module.css';
 
-const Input = ({ label, id, type, isValid, value, onChange, onBlur }) => {
+const Input = React.forwardRef(({ label, id, type, isValid, value, onChange, onBlur }, ref) => {
+  const inputRef = useRef();
+
+  const activate = () => {
+    inputRef.current.focus();
+  };
+
+  useImperativeHandle(ref,() => {
+    return {
+      focus: activate,
+    }
+  })
+
   return (
     <div
       className={`${classes.control} ${isValid === false ? classes.invalid : ''}`}
     >
       <label htmlFor={id}>{label}</label>
       <input
+        ref={inputRef}
         type={type}
         id={id}
         value={value}
@@ -16,6 +29,6 @@ const Input = ({ label, id, type, isValid, value, onChange, onBlur }) => {
       />
     </div>
   );
-};
+});
 
 export default Input;
